4fc29fad9ea52966c9a05bf2c1b290346ed0ef36
James Ravago <ravagojames@gmail.com>
2020-04-19 19:41:42 +0200

    First Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:buddylang/models/user_data.dart';
 import 'package:buddylang/screens/home_screen.dart';
 import 'package:buddylang/screens/login_screen.dart';
 import 'package:buddylang/services/auth_service.dart';
@@ -5,15 +6,12 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
-void main() => runApp(
-  MultiProvider(
-    providers: [
-      Provider<AuthService>(create: (_) => AuthService(),
-    ),
-    ], 
-    child: MyApp()
-    )
-  );
+void main() => runApp(MultiProvider(providers: [
+      ChangeNotifierProvider(create: (_) => UserData()),
+      Provider<AuthService>(
+        create: (_) => AuthService(),
+      ),
+    ], child: MyApp()));
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
@@ -29,6 +27,7 @@ class MyApp extends StatelessWidget {
         stream: Provider.of<AuthService>(context, listen: false).user,
         builder: (BuildContext context, AsyncSnapshot snapshot) {
           if (snapshot.hasData) {
+            Provider.of<UserData>(context, listen: false).currentUserId = snapshot.data.uid;
             return HomeScreen();
           } else {
             return LoginScreen();
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -9,7 +9,9 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: 'Chats'),
+      appBar: AppBar(
+        title: Text('BuddyLang'),
+      ),
     );
   }
 }
