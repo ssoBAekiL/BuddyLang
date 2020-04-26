f5ec79dab7bda183440ca57a9c1fa09b1929ea63
James Ravago <ravagojames@gmail.com>
2020-04-26 13:01:23 +0200

    Login iOS & Android

--- lib/services/auth_service.dart
+++ lib/services/auth_service.dart
@@ -1,16 +1,14 @@
 import 'package:buddylang/models/user_model.dart';
 import 'package:buddylang/utilities/constants.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_facebook_login/flutter_facebook_login.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 
-
 class AuthService {
   final FirebaseAuth _auth = FirebaseAuth.instance;
-  
+
   final FirebaseMessaging _messaging = FirebaseMessaging();
 
   Stream<FirebaseUser> get user => _auth.onAuthStateChanged;
@@ -18,7 +16,6 @@ class AuthService {
   Future<void> signup(String name, String email, String password) async {
     try {
       AuthResult authResult = await _auth.createUserWithEmailAndPassword(
-
         email: email,
         password: password,
       );
@@ -50,8 +47,6 @@ class AuthService {
     ]);
   }
 
-
-
   Future<void> removeToken() async {
     final currentUser = await _auth.currentUser();
     await usersRef
@@ -59,55 +54,51 @@ class AuthService {
         .setData({'token': ''}, merge: true);
   }
 
-
-
-
-
   Future<void> googleSignUp() async {
     try {
       final GoogleSignIn _googleSignIn = GoogleSignIn(
-        scopes: [
-          'email'
-        ],
+        scopes: ['email'],
       );
       final FirebaseAuth _auth = FirebaseAuth.instance;
 
       final GoogleSignInAccount googleUser = await _googleSignIn.signIn();
-      final GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+      final GoogleSignInAuthentication googleAuth =
+          await googleUser.authentication;
 
       final AuthCredential credential = GoogleAuthProvider.getCredential(
         accessToken: googleAuth.accessToken,
         idToken: googleAuth.idToken,
       );
 
-      final FirebaseUser user = (await _auth.signInWithCredential(credential)).user;
+      final FirebaseUser user =
+          (await _auth.signInWithCredential(credential)).user;
       print("signed in " + user.displayName);
 
       return user;
-    }catch (e) {
+    } catch (e) {
       print(e.message);
     }
   }
 
-Future<void> signOut() async {
+  Future<void> signOut() async {
     await _auth.signOut();
-   }
+  }
 
- Future<void> signUpWithFacebook() async{
+  Future<void> signUpWithFacebook() async {
     try {
       var facebookLogin = new FacebookLogin();
       var result = await facebookLogin.logIn(['email']);
 
-      if(result.status == FacebookLoginStatus.loggedIn) {
+      if (result.status == FacebookLoginStatus.loggedIn) {
         final AuthCredential credential = FacebookAuthProvider.getCredential(
           accessToken: result.accessToken.token,
-
         );
-        final FirebaseUser user = (await FirebaseAuth.instance.signInWithCredential(credential)).user;
+        final FirebaseUser user =
+            (await FirebaseAuth.instance.signInWithCredential(credential)).user;
         print('signed in ' + user.displayName);
         return user;
       }
-    }catch (e) {
+    } catch (e) {
       print(e.message);
     }
   }
